e5eb3afea543e200d6937d7c91806286773215d7
cecilesauder <cecile.sauder@gmail.com>
2019-06-21 13:57:18 +0200

    allez je le retente

---
+++ results/bazar_bibliographeR/deploy_app.R
@@ -0,0 +1,6 @@
+install.packages("rsconnect")
+library(rsconnect)
+rsconnect::setAccountInfo(name='cecilesauder', token='557317F15D5B138698A64DD8DA2FC6E6', secret='lWG2AsOatUUJW1FRiwxc3EYvFCqE36mHViyuMQXV')
+setwd("inst/BibliographeR/")
+runApp()
+deployApp()
